10bf775813294d5cbd539fd7412a4f586735079f
Ernane Sena <ernane.sena@gmail.com>
2017-02-26 15:00:06 -0300

    Configuração do plugin log4j

--- src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -1,5 +1,6 @@
 package br.com.casadocodigo.loja.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -8,6 +9,8 @@ import br.com.casadocodigo.loja.models.Product;
 @Controller
 public class ProductsController {
 
+	private static final Logger logger = Logger.getLogger(ProductsController.class);
+	
 	@RequestMapping("/produtos")
 	public String save(Product product) {
 		System.out.println("Cadastro de produto" + product);
@@ -16,6 +19,13 @@ public class ProductsController {
 
 	@RequestMapping("/produtos/form")
 	public String form() {
+		// logs debug message
+		if (logger.isDebugEnabled()) {
+			logger.debug("getWelcome is executed!");
+		}
+
+		// logs exception
+		logger.error("This is Error message");
 		return ("products/form");
 	}
 }
