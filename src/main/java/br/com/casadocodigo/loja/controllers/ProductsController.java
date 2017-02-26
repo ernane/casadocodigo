5407465cd429cf04879d5d6543a1e37bd3836e64
Ernane Sena <ernane.sena@gmail.com>
2017-02-26 13:17:06 -0300

    Criação do controlador de Produtos

---
+++ src/main/java/br/com/casadocodigo/loja/controllers/ProductsController.java
@@ -0,0 +1,21 @@
+package br.com.casadocodigo.loja.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.casadocodigo.loja.models.Product;
+
+@Controller
+public class ProductsController {
+
+	@RequestMapping("/produtos")
+	public String save(Product product) {
+		System.out.println("Cadastro de produto" + product);
+		return "procuts/ok";
+	}
+
+	@RequestMapping("/produtos/form")
+	public String form() {
+		return ("products/form");
+	}
+}
